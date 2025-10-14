a85af2ea0a45a2049f0bafdd5d585608b72d7da8
Mia <mia@github.com>
2025-10-13 22:26:13 -0400

    update: change test

--- test/index.ts
+++ test/index.ts
@@ -6,19 +6,21 @@ import { LuauState, Mutable } from "../src";
 	const state = await LuauState.createAsync({
 		lpool: Mutable({ a: { b: 1 } }),
 		test: function (arg: any) {
+			console.log(arg, arg.toString())
 			return state.env?.lpool.get(arg);
 		}
 	});
 
 	const func = state.loadstring(`
-local lbuf = buffer.create(32)
-print(lpool.a.b)
+local lbuf = {7,8,9}
+print("a.b", lpool.a.b)
 lpool[lbuf] = {4,5,6}
 lpool.a = 1000
-print(test(lbuf))
+print("test", test(lbuf))
+print("a.", lpool.a)
 `, "test/index.ts", true);
 	console.log(func());
 
-	console.log(state.env?.lpool['a'])
-	console.log("lua values:", state.env?.lpool)
+	console.log("lpool.a", state.env?.lpool['a'])
+	console.log("lpool", state.env?.lpool)
 })();
