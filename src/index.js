af1cdeaa379fea13c6f28698fe69af7ed743aae5
Mia <mia@github.com>
2025-12-12 01:06:58 -0500

    fix: es6 export for web

--- src/index.js
+++ src/index.js
@@ -1,153 +1,175 @@
-import InternalLuauWasmModule from "./lib/Luau.Web.js"
+import InternalLuauWasmModule from "./lib/Luau.Web.js";
 
 var Luau = {
-    LUA_VALUE: Symbol("LuaValue"),
-    JS_VALUE: Symbol("JsValue"),
-    JS_MUTABLE: Symbol("JsMutable"),
+  LUA_VALUE: Symbol("LuaValue"),
+  JS_VALUE: Symbol("JsValue"),
+  JS_MUTABLE: Symbol("JsMutable"),
 
-    luaValueCache: new Map(),
-    jsValueCache: new Map(),
-    jsValueReverse: new Map(),
+  luaValueCache: new Map(),
+  jsValueCache: new Map(),
+  jsValueReverse: new Map(),
 
-    transactionData: [],
-    environments: [],
+  transactionData: [],
+  environments: [],
 
-    nextJSRef: -1,
-    nextTXKey: 0
-}
+  nextJSRef: -1,
+  nextTXKey: 0,
+};
 
-var Initialized = false
+var Initialized = false;
+var InitPromise = null;
 
 async function ensureInitialized() {
-    if (!Initialized) {
-        await InternalLuauWasmModule(Luau)
-        Initialized = true
-    }
+  if (InitPromise) return InitPromise;
+  if (Initialized) return true;
 
-    return Initialized
+  InitPromise = (async () => {
+    const moduleInstance = await InternalLuauWasmModule(Luau);
+
+    Object.assign(Luau, moduleInstance);
+
+    Initialized = true;
+    return true;
+  })();
+
+  return InitPromise;
 }
 
-ensureInitialized()
-
 class CompileError extends Error {
-    constructor(message) {
-        super(message)
-        this.name = "CompileError"
-    }
+  constructor(message) {
+    super(message);
+    this.name = "CompileError";
+  }
 }
 
 function Mutable(object) {
-    if (object instanceof Map) {
-        object.set(Luau.JS_MUTABLE, true)
-        return object
-    } else {
-        const map = new Map(Object.entries(object))
-        map.set(Luau.JS_MUTABLE, true)
-        return new Proxy(map, {
-            get(target, prop) {
-                if (typeof target[prop] === "function") {
-                    return target[prop].bind(target)
-                }
-                if (target.has(prop)) {
-                    return target.get(prop)
-                }
-                return target[prop]
-            },
-            set(target, prop, value) {
-                if (typeof prop === "string") {
-                    target.set(prop, value)
-                } else {
-                    target[prop] = value
-                }
-                return true
-            }
-        })
-    }
+  if (object instanceof Map) {
+    object.set(Luau.JS_MUTABLE, true);
+    return object;
+  } else {
+    const map = new Map(Object.entries(object));
+    map.set(Luau.JS_MUTABLE, true);
+    return new Proxy(map, {
+      get(target, prop) {
+        if (typeof target[prop] === "function") {
+          return target[prop].bind(target);
+        }
+        if (target.has(prop)) {
+          return target.get(prop);
+        }
+        return target[prop];
+      },
+      set(target, prop, value) {
+        if (typeof prop === "string") {
+          target.set(prop, value);
+        } else {
+          target[prop] = value;
+        }
+        return true;
+      },
+    });
+  }
 }
 
 class LuauState {
-    static async createAsync(env) {
-        await ensureInitialized()
+  static async createAsync(env) {
+    await ensureInitialized();
 
-        const instance = new LuauState(env)
-        return instance
+    const instance = new LuauState(env);
+    return instance;
+  }
+
+  constructor(env) {
+    if (!Initialized) {
+      throw new Error(
+        "Luau not initialized. Use LuauState.createAsync() instead of new LuauState()"
+      );
     }
 
-    constructor(env) {
-        if (!Initialized) {
-            throw new Error("Luau not initialized. Use LuauState.createAsync() instead of new LuauState()")
-        }
-
-        if (env) {
-            this.env = env
-            Luau.environments = Luau.environments || []
-            this.envIdx = Luau.environments.length + 1
-            Luau.environments[this.envIdx] = this.env
-        } else {
-            this.envIdx = 0
-        }
-
-        this.destroyed = false;
-        this.state = Luau.ccall('makeLuaState', 'int', ['number'], [this.envIdx])
+    if (env) {
+      this.env = env;
+      Luau.environments = Luau.environments || [];
+      this.envIdx = Luau.environments.length + 1;
+      Luau.environments[this.envIdx] = this.env;
+    } else {
+      this.envIdx = 0;
     }
 
-    setEnvironment(env) {
-        if (this.destroyed) {
-            throw new CompileError("Cannot use destroyed Luau state")
-        }
+    this.destroyed = false;
+    this.state = Luau.ccall("makeLuaState", "int", ["number"], [this.envIdx]);
+  }
 
-        Luau.environments[this.envIdx] = env
+  setEnvironment(env) {
+    if (this.destroyed) {
+      throw new CompileError("Cannot use destroyed Luau state");
     }
 
-    getValue(idx) {
-        if (this.destroyed) {
-            throw new CompileError("Cannot use destroyed Luau state")
-        }
+    Luau.environments[this.envIdx] = env;
+  }
 
-        var transactionId = Luau.ccall('getLuaValue', 'int', ['number', 'number'], [this.state, idx])
-        var luauValue = null
-        try {
-            luauValue = JSON.parse(Luau.transactionData[transactionId])
-        } catch (e) { }
-        return Luau.luauToJsValue(this.state, luauValue)
+  getValue(idx) {
+    if (this.destroyed) {
+      throw new CompileError("Cannot use destroyed Luau state");
     }
 
-    makeTransaction(value) {
-        if (this.destroyed) {
-            throw new CompileError("Cannot use destroyed Luau state")
-        }
+    var transactionId = Luau.ccall(
+      "getLuaValue",
+      "int",
+      ["number", "number"],
+      [this.state, idx]
+    );
+    var luauValue = null;
+    try {
+      luauValue = JSON.parse(Luau.transactionData[transactionId]);
+    } catch (e) {}
+    return Luau.luauToJsValue(this.state, luauValue);
+  }
 
-        const idx = Luau.nextTXKey++
-        Luau.transactionData[idx] = value
-
-        return idx
+  makeTransaction(value) {
+    if (this.destroyed) {
+      throw new CompileError("Cannot use destroyed Luau state");
     }
 
-    loadstring(source, chunkname = "LuauWeb", throwOnCompilationError = false) {
-        if (this.destroyed) {
-            throw new CompileError("Cannot use destroyed Luau state")
-        }
+    const idx = Luau.nextTXKey++;
+    Luau.transactionData[idx] = value;
 
-        const loadStatus = Luau.ccall('luauLoad', 'int', ['number', 'number', 'number'], [this.state, this.makeTransaction(source), this.makeTransaction(chunkname)])
-        if (loadStatus != 0) {
-            const error = this.getValue(-1)
-            if (throwOnCompilationError) {
-                throw new CompileError(error)
-            }
-            return error
-        }
+    return idx;
+  }
 
-        return this.getValue(-1)
+  loadstring(source, chunkname = "LuauWeb", throwOnCompilationError = false) {
+    if (this.destroyed) {
+      throw new CompileError("Cannot use destroyed Luau state");
     }
 
-    destroy() {
-        if (this.destroyed) {
-            throw new CompileError("Cannot use destroyed Luau state")
-        }
-
-        this.destroyed = true;
-        Luau.ccall('luauClose', 'void', ['int'], [this.state])
+    const loadStatus = Luau.ccall(
+      "luauLoad",
+      "int",
+      ["number", "number", "number"],
+      [
+        this.state,
+        this.makeTransaction(source),
+        this.makeTransaction(chunkname),
+      ]
+    );
+    if (loadStatus != 0) {
+      const error = this.getValue(-1);
+      if (throwOnCompilationError) {
+        throw new CompileError(error);
+      }
+      return error;
     }
+
+    return this.getValue(-1);
+  }
+
+  destroy() {
+    if (this.destroyed) {
+      throw new CompileError("Cannot use destroyed Luau state");
+    }
+
+    this.destroyed = true;
+    Luau.ccall("luauClose", "void", ["int"], [this.state]);
+  }
 }
 
-export { Mutable, LuauState, CompileError, Luau as InternalLuauWasmModule }
+export { Mutable, LuauState, CompileError, Luau as InternalLuauWasmModule };
